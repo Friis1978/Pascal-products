a174aded4206bbf2389251dfe5f3d3e9c677f00e
Rasmus Friis <friis1978@gmail.com>
2024-06-01 11:34:45 +0200

    remove from basket

--- pages/index.vue
+++ pages/index.vue
@@ -29,40 +29,53 @@
       </ul>
     </div>
 
-    <div
-      v-if="currentProducts"
-      class="border border-blue-500 p-2 rounded-lg mt-10 h-[21rem]"
-    >
-      <ul class="text-xl max-h-[20rem] overflow-scroll scrollbar">
-        <li
-          v-for="(item, index) in currentProducts"
-          :key="index"
-          @click="selectedProduct = item"
-          class="hover:text-blue-500 hover:cursor-pointer"
-        >
-          {{ `${index + 1}: ${item.product}` }}
-        </li>
-      </ul>
-    </div>
-    <div
-      v-if="selectedProduct"
-      class="flex flex-col justify-between bg-blue-200 p-10 h-[21rem] mt-10 border border-blue-500 rounded-xl"
-      @click="addToBasket(selectedProduct)"
-    >
-      <div>
-        <p>Product: {{ selectedProduct.product }}</p>
-        <p>Series: {{ selectedProduct.series }}</p>
-        <p>company: {{ selectedProduct.company }}</p>
-        <p>Uid: {{ selectedProduct.id }}</p>
+    <div v-if="currentProducts">
+      <h1 class="text-center text-3xl">Products</h1>
+      <div class="border border-blue-500 p-2 rounded-lg mt-10 h-[21rem]">
+        <ul class="text-xl max-h-[20rem] overflow-scroll scrollbar">
+          <li
+            v-for="(item, index) in currentProducts"
+            :key="index"
+            @click="selectProduct(item)"
+            class="hover:text-blue-500 hover:cursor-pointer"
+          >
+            {{ `${index + 1}: ${item.product}` }}
+          </li>
+        </ul>
       </div>
-      <div class="flex justify-end">
-        <button class="bg-blue-500 text-white px-5 py-2 mt-5 rounded-xl">
-          Add to basket
-        </button>
+    </div>
+
+    <div v-if="selectedProduct">
+      <h1 class="text-center text-3xl">Selected</h1>
+      <div
+        class="flex flex-col justify-between bg-blue-200 p-10 h-[21rem] mt-10 border border-blue-500 rounded-xl"
+      >
+        <div>
+          <p>Product: {{ selectedProduct.product }}</p>
+          <p>Series: {{ selectedProduct.series }}</p>
+          <p>company: {{ selectedProduct.company }}</p>
+          <p>Uid: {{ selectedProduct.id }}</p>
+        </div>
+        <div class="flex justify-end">
+          <button
+            class="text-white px-5 py-2 mt-5 rounded-xl"
+            :class="inBasket ? 'bg-red-500' : 'bg-blue-500'"
+            @click="
+              () => {
+                !inBasket
+                  ? addToBasket(selectedProduct)
+                  : removeFromBasket(selectedProduct.id);
+              }
+            "
+          >
+            {{ inBasket ? "Remove from basket" : "Add to basket" }}
+          </button>
+        </div>
       </div>
     </div>
   </div>
   <div>
+    <h1 class="text-center text-3xl w-1/4">Basket</h1>
     <ul class="p-10 text-xl">
       <li
         v-for="(item, index) in basket"
@@ -70,7 +83,12 @@
         class="hover:text-blue-500 hover:cursor-pointer"
       >
         {{ `${index + 1}: ${item.product}` }}
-        <button class="bg-blue-500 text-sm text-white px-2 py-1 mt-5 rounded-xl"> remove </button>
+        <button
+          class="bg-blue-500 text-sm text-white px-2 py-1 mt-5 rounded-xl"
+          @click="removeFromBasket(item.id)"
+        >
+          remove
+        </button>
       </li>
     </ul>
   </div>
@@ -89,6 +107,7 @@ const currentCompany = ref();
 const currentCategory = ref();
 
 const selectedProduct = ref();
+const inBasket = ref(false);
 
 const basket = ref([]);
 
@@ -98,6 +117,11 @@ const getCompanies = () => {
 };
 
 const getCategories = (id) => {
+  // Clear selected
+  currentProducts.value = undefined;
+  currentCategory.value = undefined;
+  selectedProduct.value = undefined;
+
   currentCompany.value = id;
 
   const allProducts = data.filter((val) => val.company === id);
@@ -109,17 +133,37 @@ const getCategories = (id) => {
 };
 
 const getProducts = (id) => {
+  // Clear selected
+  selectedProduct.value = undefined;
+
   currentCategory.value = id;
   const categoryProducts = companyProducts.value.filter(
     (val) => val.series === id
   );
-  console.log("category products", id, currentProducts, categoryProducts);
   currentProducts.value = categoryProducts;
+};
+
+const selectProduct = (item) => {
+  selectedProduct.value = item;
+  isInBasket(item.id);
 };
 
 const addToBasket = (product) => {
   basket.value = [...basket.value, product];
-  console.log(basket.value);
+  selectedProduct.value = undefined;
+};
+
+const removeFromBasket = (id) => {
+  const newlist = basket.value.filter((val) => val.id !== id);
+  basket.value = newlist;
+  isInBasket(id);
+};
+
+const isInBasket = (id) => {
+  // find in basket
+  const isPresent = basket?.value?.filter((val) => val.id === id);
+  if (isPresent?.length) inBasket.value = true;
+  else inBasket.value = false;
 };
 
 onMounted(() => {
